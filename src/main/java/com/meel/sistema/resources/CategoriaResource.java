04232b303d693c31e5eb90de5086b090de7d61ab
jerameelcode <jerameelcode@users.noreply.github.com>
2020-07-24 16:21:29 -0300

    Domain: Categoria

--- src/main/java/com/meel/sistema/resources/CategoriaResource.java
+++ src/main/java/com/meel/sistema/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.meel.sistema.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.meel.sistema.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categoria")
+@RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	
@@ -14,8 +19,10 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Primeiro recurso de Categorias.";
+	public List<Categoria> listar() {
+		Categoria informatica = new Categoria(1, "Informática");
+		Categoria escritorio = new Categoria(1, "Escritório");
+		return Arrays.asList(informatica, escritorio);
 	}
 
 }
