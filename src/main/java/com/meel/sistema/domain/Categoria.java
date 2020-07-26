1e3155d141fe80aca0e8829a08e7802b732998b6
jerameelcode <jerameelcode@users.noreply.github.com>
2020-07-26 20:53:21 -0300

    Categoria CRUD implemented

--- src/main/java/com/meel/sistema/domain/Categoria.java
+++ src/main/java/com/meel/sistema/domain/Categoria.java
@@ -6,9 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Categoria implements Serializable{
@@ -19,8 +18,7 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	@NotBlank
-	@Min(value = 10, message = "O Nome da categoria tem que ser maior que 10 caracteres")
-	@Max(value = 30, message = "O Nome da categoria tem que ser menor que 30 caracteres")
+	@Size(max = 30, message = "O nome da categoria tem que ser maior que 10 caracteres")
 	private String nome;
 	
 	public Categoria() {}
--- src/main/java/com/meel/sistema/resources/CategoriaResource.java
+++ src/main/java/com/meel/sistema/resources/CategoriaResource.java
@@ -5,10 +5,13 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,6 +41,9 @@ public class CategoriaResource {
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Categoria> findById(@PathVariable(name = "id") Integer id) {
 		Optional<Categoria> categoria = this.categoriaRepository.findById(id);
+		if(!categoria.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
 		return ResponseEntity.ok(categoria.get());
 		
 	}
@@ -49,6 +55,28 @@ public class CategoriaResource {
 			return ResponseEntity.ok(savedCategoria);
 		} catch (Exception e) {
 			throw e;
+		}
+	}
+	
+	@PutMapping(value="/")
+	public ResponseEntity<Categoria> update(@RequestBody Categoria categoria){
+		Optional<Categoria> cat = this.categoriaRepository.findById(categoria.getId());
+		if(cat.isPresent()) {
+			Categoria updateCategoria = this.categoriaRepository.save(categoria);
+			return ResponseEntity.ok(updateCategoria);
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Categoria> delete(@PathVariable(name = "id") Integer id) {
+		Optional<Categoria> categoria = this.categoriaRepository.findById(id);
+		if(categoria.isPresent()) {
+			this.categoriaRepository.delete(categoria.get());
+			return ResponseEntity.status(HttpStatus.GONE).build();
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
 	}
 
